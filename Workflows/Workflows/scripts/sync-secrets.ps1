a7072c2ec8d7b2d3ee1cf064f889848abbb42ba2
vlad <vlad19972017@gmail.com>
2025-05-31 10:45:38 +0300

    Update sync script

--- Workflows/Workflows/scripts/sync-secrets.ps1
+++ Workflows/Workflows/scripts/sync-secrets.ps1
@@ -1,5 +1,29 @@
 $ErrorActionPreference = "Stop"
 
+function EnsureSecretsPathExists {
+    <#
+    .SYNOPSIS
+    Ensures that the directory for the secrets file exists.
+
+    .DESCRIPTION
+    This function ensures that the directory for the secrets file exists.
+
+    .PARAMETER SecretsFilePath
+    The full path to the secrets file.
+
+    .EXAMPLE
+    EnsureSecretsPathExists -SecretsFilePath "C:\Path\To\Your\Secrets.json"
+    #>
+    param (
+         [Parameter(Mandatory=$true)]$SecretsFilePath
+    )
+
+    if (-not (Test-Path -Path $SecretsFilePath)) {
+        Write-Host "Creating secrets file at $SecretsFilePath"
+        New-Item -Path $SecretsFilePath -ItemType File -Force | Out-Null
+    }
+}
+
 # This script syncs user secrets from a .csproj file to the user's secrets store.
 Write-Host "Running script: $($MyInvocation.MyCommand.Source)" -ForegroundColor Green
 
@@ -9,9 +33,9 @@ $CsprojPath = "$Location\..\$ProjectName.csproj"
 
 Write-Host "Syncing secrets from $CsprojPath" -ForegroundColor Green
 
-if (-not (Test-Path -IsValid $CsprojPath)) {
+if (-not (Test-Path $CsprojPath)) {
     Write-Host "Could not find csproj file at $CsprojPath" -ForegroundColor Red
-    exit 1
+    return
 }
 
 [xml]$CsprojPathXml = Get-Content $CsprojPath
@@ -19,7 +43,7 @@ $SecretId = $CsprojPathXml.Project.PropertyGroup.UserSecretsId
 
 if ($null -eq $SecretId) {
     Write-Host "UserSecretsId not found in csproj file." -ForegroundColor Red
-    exit 1
+    return
 }
 
 Write-Host "UserSecretsId: $SecretId"
@@ -35,10 +59,14 @@ $SecretsConfig.Config.Data2 = "Data2";
 
 $SecretsConfig.Config.Data3 = @("Data3", "Data4", "Data5");
 
-$SecretsPath = "$env:APPDATA\Microsoft\UserSecrets\$SecretId\secrets.json"
-Write-Host "Saving secrets to $SecretsPath"
+$SecretsDirectory = "$env:APPDATA\Microsoft\UserSecrets\$SecretId"
+$SecretsFilePath = "$SecretsDirectory\secrets.json"
+
+EnsureSecretsPathExists -SecretsFilePath $SecretsFilePath
+
+Write-Host "Saving secrets to $SecretsFilePath"
 
 $json = $SecretsConfig | ConvertTo-Json -Depth 10;
-Set-Content -Path $SecretsPath -Value $json -Encoding UTF8
+Set-Content -Path $SecretsFilePath -Value $json -Encoding UTF8
 
 Write-Host "Secrets synced successfully." -ForegroundColor Green
